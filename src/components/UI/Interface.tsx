f53c4316d28c36a5b0ed19a565906db86ad1f7c4
Ryan Henrique <ryanhardflip@gmail.com>
2023-09-02 00:12:59 -0300

    feat: adding skills

--- src/components/UI/Interface.tsx
+++ src/components/UI/Interface.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const Section = (props: any) => {
   const { children } = props;
 
@@ -49,19 +51,86 @@ const AboutSection = () => {
     </Section>
   );
 };
+
+const skills = [
+  {
+    title: "Threejs / React Three Fiber",
+    level: 80,
+  },
+  {
+    title: "React / React Native",
+    level: 90,
+  },
+  {
+    title: "Nodejs",
+    level: 80,
+  },
+  {
+    title: "Typescript",
+    level: 60,
+  },
+];
+
+const SkillSection = () => {
+  return (
+    <Section>
+      <motion.div whileInView={"visible"} className="pl-10">
+        <h2 className="text-5xl font-bold text-white">Skills</h2>
+        <div className=" mt-8 space-y-4">
+          {skills.map((skill, index) => (
+            <div className="w-64" key={index}>
+              <motion.h3
+                className="text-xl font-bold text-white"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
+              <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                <motion.div
+                  className="h-full bg-[#B30DEE] rounded-full "
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-5xl font-bold mt-10 text-white">Languages</h2>
+        </div>
+      </motion.div>
+    </Section>
+  );
+};
+
 export const Interface = () => {
   return (
     <div className="flex flex-col items-center w-screen">
       <AboutSection />
-      <Section>
-        <h1>HOME</h1>
-      </Section>
-      <Section>
-        <h1>HOME</h1>
-      </Section>
-      <Section>
-        <h1>HOME</h1>
-      </Section>
+      <SkillSection />
     </div>
   );
 };
