45e0f305e2e8f240666654fb51c1fdd32d28b4cc
Ryan Henrique <ryanhardflip@gmail.com>
2023-09-11 15:20:39 -0300

    feat

--- src/components/UI/Interface.tsx
+++ src/components/UI/Interface.tsx
@@ -142,189 +142,170 @@ const AboutSection = () => {
     <Section>
       <div className="flex flex-col w-screen h-screen justify-center items-center">
         <Header />
-        <div className="w-full h-full flex justify-center items-center">
-          <motion.div
-            data-testid={"div"}
-            whileInView={"visible"}
-            initial={{
-              opacity: 0,
-            }}
-            variants={{
-              visible: {
-                opacity: 1,
-                transition: {
-                  duration: 1,
-                  delay: 0,
-                },
-              },
-            }}
-            className="w-screen h-1/2 flex flex-col justify-center items-center gap-4"
-          >
-            <motion.header
-              initial={{ x: -250 }}
-              whileInView={{ x: 0 }}
-              transition={{
-                duration: 1,
-                delay: 0,
+        <div className="lg:flex flex-col w-full h-full justify-center items-center">
+          <div className="lg:w-1/2 w-full  lg:h-full h-1/2 flex justify-center items-center bg-red-600/10 lg:pl-10 text-white">
+            <motion.div
+              data-testid={"div"}
+              whileInView={"visible"}
+              initial={{
+                opacity: 0,
               }}
-              className="w-full flex justify-center items-center lg:justify-start text-white font-black text-lg lg:h-32 lg:items-end 2xl:h-64"
+              variants={{
+                visible: {
+                  opacity: 1,
+                  transition: {
+                    duration: 1,
+                    delay: 0,
+                  },
+                },
+              }}
+              className="w-full h-full flex justify-center items-center flex-col gap-2"
             >
-              <div className="flex flex-col justify-center text-sm min-[700px]:text-2xl lg:pl-12 xl:pl-16 pl-0 2xl:text-3xl">
-                <span className="lg:text-xl ">👋 OLÁ, EU SOU</span>
-                <span className="lg:text-xl ">RYAN HENRIQUE</span>
-              </div>
-            </motion.header>
-            <div className="w-full flex flex-col justify-center items-center">
-              <motion.div
+              <motion.header
                 initial={{ x: -250 }}
                 whileInView={{ x: 0 }}
                 transition={{
                   duration: 1,
                   delay: 0,
                 }}
-                className="flex w-full h-fit justify-center items-center lg:items-start lg:pl-12 xl:pl-14 flex-col text-white font-black lg:pt-6 2xl:pt-10"
+                className=""
               >
-                <div className="italic">
-                  <h1 className="text-3xl md:text-5xl min-[600px]:text-3xl  min-[1000px]:text-5xl xl:text-6xl 2xl:text-7xl">
-                    DESENVOLVEDOR
-                  </h1>
+                <div className="flex flex-col justify-center text-sm font-black">
+                  <span className="lg:text-xl ">👋 OLÁ, EU SOU</span>
+                  <span className="lg:text-xl ">RYAN HENRIQUE</span>
                 </div>
-                <div className="flex gap-2 md:gap-3 2xl:gap-6">
+              </motion.header>
+              <div className="w-full flex flex-col justify-center items-center text-center">
+                <motion.div
+                  initial={{ x: -250 }}
+                  whileInView={{ x: 0 }}
+                  transition={{
+                    duration: 1,
+                    delay: 0,
+                  }}
+                  className="w-full flex flex-col justify-center items-center text-4xl font-black"
+                >
                   <div className="italic">
-                    <h1 className="text-3xl md:text-5xl min-[600px]:text-3xl  min-[1000px]:text-5xl xl:text-6xl 2xl:text-7xl">
-                      WEB
-                    </h1>
+                    <h1 className="">DESENVOLVEDOR</h1>
                   </div>
-                  <div>
-                    <h1
-                      ref={colorRef}
-                      className={`tracking-wide text-3xl md:text-5xl min-[600px]:text-3xl  min-[1000px]:text-5xl xl:text-6xl 2xl:text-7xl`}
-                    >
-                      FRONT-END
-                    </h1>
+                  <div className="flex gap-2 md:gap-3 2xl:gap-6">
+                    <div className="italic">
+                      <h1 className="">WEB</h1>
+                    </div>
+                    <div>
+                      <h1 ref={colorRef} className="">
+                        FRONT-END
+                      </h1>
+                    </div>
                   </div>
-                </div>
-              </motion.div>
+                </motion.div>
+                <motion.div
+                  data-testid={"about1"}
+                  initial={{ x: -250 }}
+                  whileInView={{ x: 0 }}
+                  transition={{
+                    duration: 1,
+                    delay: 0,
+                  }}
+                  className="w-[80%] text-sm min-[400px]:text-md min-[450px]:pt-4"
+                >
+                  <p>
+                    Sou um jovem desenvolvedor muito criativo, interessado em
+                    criar experiências de usuario incríveis, agradáveis,
+                    escaláveis <br />e performáticas. Busco o crescimento
+                    profissional e pessoal.
+                  </p>
+                  <p>
+                    possuo habilidade de adaptabilidade, comunicação clara,
+                    <br /> e uma intensa vontade de aprender.
+                  </p>
+                  <p className="lg:text-left">
+                    Que tal trabalharmos juntos? 😄
+                  </p>
+                </motion.div>
+              </div>
+            </motion.div>
+          </div>
+          <div className="lg:w-1/2 w-full lg:h-full h-1/2 flex justify-center items-center bg-green-600/10">
+            <motion.div
+              whileInView={"visible"}
+              initial={{
+                opacity: 0,
+              }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                  transition: {
+                    duration: 1,
+                    delay: 0,
+                  },
+                },
+              }}
+              className="w-full h-full flex flex-col items-center justify-center"
+            >
               <motion.div
-                data-testid={"about1"}
-                initial={{ x: -250 }}
-                whileInView={{ x: 0 }}
+                initial={{ opacity: 0, x: 200 }}
+                whileInView={{ opacity: 1, x: 0 }}
                 transition={{
                   duration: 1,
                   delay: 0,
                 }}
-                className="w-[90%] text-center lg:text-left flex flex-col justify-center items-center lg:items-start gap-6 text-md md:text-lg md:pt-4 text-white font-medium pt-8 min-[900px]:pl-8 lg:pl-6 lg:text-lg lg:pt-14 xl:text-lg xl:pl-8 2xl:text-3xl"
+                className="text-white font-medium text-center md:text-center text-md flex flex-col items-center"
               >
-                <p>
-                  Sou um jovem desenvolvedor muito criativo, interessado em
-                  criar experiências de usuario incríveis, agradáveis,
-                  escaláveis <br />e performáticas. Busco o crescimento
-                  profissional e pessoal.
-                </p>
-                <p>
-                  possuo habilidade de adaptabilidade, comunicação clara,
-                  <br /> e uma intensa vontade de aprender.
-                </p>
-                <p className="lg:text-left">Que tal trabalharmos juntos? 😄</p>
-              </motion.div>
-            </div>
-          </motion.div>
-          <motion.div
-            whileInView={"visible"}
-            initial={{
-              opacity: 0,
-            }}
-            variants={{
-              visible: {
-                opacity: 1,
-                transition: {
-                  duration: 1,
-                  delay: 0,
-                },
-              },
-            }}
-            className="w-screen flex h-1/2 flex-col justify-center items-center gap-4"
-          >
-            <motion.div
-              initial={{ opacity: 0, x: 200 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{
-                duration: 1,
-                delay: 0,
-              }}
-              className="w-full flex items-center justify-center lg:justify-end text-lg font-black text-white md:pr-10 gap-2 min-[900px]:pr-14 lg:pr-20 lg:h-32 2xl:h-64 2xl:pt-40"
-            >
-              <span className="md:text-lg">COLOR:</span>
-              <input
-                data-testid={"input-color"}
-                className="w-6 h-6  border-0 bg-transparent cursor-pointer"
-                type="color"
-                value={color}
-                onChange={(e) => setColor(e.target.value)}
-              />
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0, x: 200 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{
-                duration: 1,
-                delay: 0,
-              }}
-              className="text-white font-medium text-center md:text-center text-md flex flex-col items-center"
-            >
-              <p
-                data-testid={"about2"}
-                className="w-[90%] md:text-lg lg:text-lg lg:text-right lg:pr-10 xl:text-lg 2xl:text-3xl"
-              >
-                Há cerca de 10 meses eu venho me dedicando aos estudos de
-                programação 🧑‍💻, sempre aplicando em projetos pessoais os
-                conceitos aprendidos. possuo habilidades no Frontend e
-                conhecimentos básicos de Backend.
-              </p>
-              <motion.div className="w-full h-full flex flex-col lg:items-end lg:pr-20 xl:pt-20 items-center justify-center md:justify-center md:pt-0 md:mt-4 pt-10 gap-4 lg:text-lg lg:gap-6 text-xs md:text-lg font-extrabold xl:text-xl 2xl:text-3xl">
-                <motion.button
-                  whileHover={{
-                    x: 5,
-                    transition: { duration: 0.5 },
-                  }}
-                  className="text-xs xl:text-xl md:text-lg bg-white p-3 text-zinc-700 rounded-md flex gap-2 tracking-wide justify-center items-center"
+                <p
+                  data-testid={"about2"}
+                  className="w-[80%] text-sm min-[400px]:text-md "
                 >
-                  <a
-                    href="https://www.linkedin.com/in/ryan-henrique-1b4075233/"
-                    target="blank"
+                  Há cerca de 10 meses eu venho me dedicando aos estudos de
+                  programação 🧑‍💻, sempre aplicando em projetos pessoais os
+                  conceitos aprendidos. possuo habilidades no Frontend e
+                  conhecimentos básicos de Backend.
+                </p>
+                <motion.div className="pt-4 flex flex-col items-center justify-center gap-4">
+                  <motion.button
+                    whileHover={{
+                      x: 5,
+                      transition: { duration: 0.5 },
+                    }}
+                    className="text-xs xl:text-xl md:text-lg bg-white p-3 text-zinc-700 rounded-md flex gap-2 tracking-wide justify-center items-center"
                   >
-                    VIEW MY LINKEDIN
-                  </a>
-                  <img className="w-6 opacity-80" src="images/linkedin.png" />
-                </motion.button>
-                <motion.button
-                  whileHover={{
-                    x: 5,
-                    transition: { duration: 0.5 },
-                  }}
-                  className="text-xs xl:text-xl md:text-lg bg-white p-3 text-zinc-700 rounded-md flex gap-2 tracking-wide justify-center items-center"
-                >
-                  <a href="https://github.com/Ryanzitto/" target="blank">
-                    VIEW MY GITHUB
-                  </a>
-                  <img className="w-6 opacity-80" src="images/github.png" />
-                </motion.button>
+                    <a
+                      href="https://www.linkedin.com/in/ryan-henrique-1b4075233/"
+                      target="blank"
+                    >
+                      VIEW MY LINKEDIN
+                    </a>
+                    <img className="w-6 opacity-80" src="images/linkedin.png" />
+                  </motion.button>
+                  <motion.button
+                    whileHover={{
+                      x: 5,
+                      transition: { duration: 0.5 },
+                    }}
+                    className="text-xs xl:text-xl md:text-lg bg-white p-3 text-zinc-700 rounded-md flex gap-2 tracking-wide justify-center items-center"
+                  >
+                    <a href="https://github.com/Ryanzitto/" target="blank">
+                      VIEW MY GITHUB
+                    </a>
+                    <img className="w-6 opacity-80" src="images/github.png" />
+                  </motion.button>
 
-                <div className="flex md:text-2xl xl:text-2xl items-center justify-center gap-2 text-lg lg:text-xl">
-                  <img
-                    onClick={like}
-                    className="w-6 h-6 cursor-pointer"
-                    src={
-                      likeIsClicked === false
-                        ? "images/heart.png"
-                        : "images/heart-filled.png"
-                    }
-                  />
-                  <span>432 LIKES</span>
-                </div>
+                  <div className="flex md:text-2xl xl:text-2xl items-center justify-center gap-2 text-lg lg:text-xl">
+                    <img
+                      onClick={like}
+                      className="w-6 h-6 cursor-pointer"
+                      src={
+                        likeIsClicked === false
+                          ? "images/heart.png"
+                          : "images/heart-filled.png"
+                      }
+                    />
+                    <span>432 LIKES</span>
+                  </div>
+                </motion.div>
               </motion.div>
             </motion.div>
-          </motion.div>
+          </div>
         </div>
       </div>
     </Section>
@@ -402,7 +383,7 @@ const SkillSection = () => {
                 {skills.map((skill, index) => (
                   <div className="w-64" key={index}>
                     <motion.h3
-                      className="text-xl font-bold text-white"
+                      className="text-xl font-bold"
                       initial={{
                         opacity: 0,
                       }}
@@ -463,7 +444,7 @@ const SkillSection = () => {
                 {languages.map((lng, index) => (
                   <div className="w-64" key={index}>
                     <motion.h3
-                      className="text-xl font-bold text-white"
+                      className="text-xl font-bold"
                       initial={{
                         opacity: 0,
                       }}
